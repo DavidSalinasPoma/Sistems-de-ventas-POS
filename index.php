e0b627efcb3e95555d6f5d1fe32fa2854e082955
DavidSalinasPoma <favianita2005@gmail.com>
2021-02-14 10:17:58 -0400

    Creacion de controller y modelos de las vistas

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
-// Se require una sola vez de de la carpeta controllers
+// Controllers en el index.php
 require_once("mvc/controllers/plantilla.controlador.php");
 
+require_once __DIR__ . './mvc/controllers/plantilla.controlador.php';
+require_once __DIR__ . './mvc/controllers/ventas.controlador.php';
+require_once __DIR__ . './mvc/controllers/productos.controlador.php';
+require_once __DIR__ . './mvc/controllers/reportes.controlador.php';
+require_once __DIR__ . './mvc/controllers/usuarios.controlador.php';
 
+
+// Modelos en el index. php
+require_once __DIR__ . './mvc/models/ventas.model.php';
+require_once __DIR__ . './mvc/models/productos.model.php';
+require_once __DIR__ . './mvc/models/reportes.model.php';
+require_once __DIR__ . './mvc/models/usuarios.model.php';
+
+
+// Instaciamos de las clase controlador
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
--- mvc/controllers/plantilla.controlador.php
+++ mvc/controllers/plantilla.controlador.php
@@ -7,7 +7,6 @@ class ControladorPlantilla
     {
         // Incluye paginas completas html y php
         // include("mvc/views/view-plantilla/plantilla.php");
-
         include __DIR__ . './../views/view-plantilla/plantilla.php';
     }
 }
